23646a4d102d35348dbe89c8a2ead9b5462bd8be
Dylan Heathcote <c19434966@mytudublin.ie>
2021-04-21 21:26:28 +0100

    Hard coded value to each letters and commented out some code

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -26,15 +26,14 @@ public class ScoreDisplay extends PApplet
 	public void setup() 	
 	{
 		loadScore();
-		colorMode(HSB);
 	}
 
 	public void draw()
 	{
 		background(255);
+
 		float border = 0.1f * width;
 
-		
 		for(int i = 0 ; i<5 ; i++ ){
 			float y = map(i , -5 , 5 , border , height - border);
 			line(border, y, width - border, y);
@@ -42,26 +41,51 @@ public class ScoreDisplay extends PApplet
 	}
 		
 
-	void drawNotes()
+	public void drawNotes()
 	{
+		int x;
+		char c;
+
+
+
+		switch (c) {
+			case 'D':
+				x = 0;
+				break;
+			case 'E':
+				x = 1;
+				break;
+			case 'F':
+				x = 3;
+				break;
+			case 'G':
+				x = 4;
+				break;
+			case 'A':
+				x = 5;
+				break;
+			case 'B':
+				x = 6;
+				break;
+			case 'c':
+				x = 7;
+				break;
+			case 'd':
+				x = 8;
+				break;
 
 	}
 
  	void loadScore()
  {
-	String s = score.substring(0, 8);
-	int i = 0;
+	//String s = score.substring(0, 8);
+	//int i = 0;
 
-	while( i < score.length())
+	/*while( i < score.length())
 	{
 		if(Character.isLetter(score.charAt(i))) //Not sure if isLetter should be used here, basically makes sure its a character that gets passed.
 		
 	}
-
-	for(int i = s.length() - 1; i >= 0; i--)
-	{
-		System.out.println(s.charAt(i));
-	}
-
+*/
  }
 }
